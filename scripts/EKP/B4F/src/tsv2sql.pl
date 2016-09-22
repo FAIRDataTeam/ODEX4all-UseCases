b264ff8c9d82e8bf35f7dfdc254390b943987331
arnikz <a.kuzniar@esciencecenter.nl>
2016-09-22 11:29:13 +0200

    Update doc

--- scripts/EKP/B4F/src/tsv2sql.pl
+++ scripts/EKP/B4F/src/tsv2sql.pl
@@ -2,7 +2,7 @@
 
 use strict;
 
-my $table = $ARGV[0] || 'B4F.odex4all.QTL';
+my $table = $ARGV[0] || 'B4F.odex4all.QTL'; # catalog.schema.name in Virtuoso jargon
 my $header = <>; # first line must be a header
 chomp($header);
 my $colist = '"' . join('","', split /\t/, $header) . '"'; # ordered list of collumn names
