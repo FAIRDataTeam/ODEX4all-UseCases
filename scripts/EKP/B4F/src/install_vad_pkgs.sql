177513d2338281fd98d0464c56ab7ea6e934d155
arnikz <a.kuzniar@esciencecenter.nl>
2016-09-23 19:33:16 +0200

    Cosmetic changes

--- scripts/EKP/B4F/src/install_vad_pkgs.sql
+++ scripts/EKP/B4F/src/install_vad_pkgs.sql
@@ -1,5 +1,5 @@
-set u{path} /usr/local/virtuoso-opensource/share/virtuoso/vad/;
-vad_install('$u{path}/rdb2rdf_dav.vad', 0);
-vad_install('$u{path}/rdf_mappers_dav.vad', 0);
-vad_install('$u{path}/fct_dav.vad', 0);
-quit;
+SET u{path} /usr/local/virtuoso-opensource/share/virtuoso/vad/;
+VAD_INSTALL('$u{path}/rdb2rdf_dav.vad', 0);
+VAD_INSTALL('$u{path}/rdf_mappers_dav.vad', 0);
+VAD_INSTALL('$u{path}/fct_dav.vad', 0);
+QUIT;
