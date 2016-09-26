943cd1118cae07321390d6190cecd6776baf3d74
arnikz <a.kuzniar@esciencecenter.nl>
2016-09-26 17:19:32 +0200

    Change db schema: rename QTL table columns start->start_pos and end->end_pos

--- scripts/EKP/B4F/src/create_db.sql
+++ scripts/EKP/B4F/src/create_db.sql
@@ -4,27 +4,27 @@ CREATE TABLE "B4F"."odex4all"."QTL" (
    "chromosome"		VARCHAR,
    "source"		VARCHAR,
    "feature_type"	VARCHAR,
-   "start" 		INTEGER,
-   "end" 		INTEGER,
+   "start_pos"		INTEGER,
+   "end_pos"		INTEGER,
    "additive_effect"	FLOAT,
-   "dominance_effect" 	FLOAT,
-   "bayes_value" 	FLOAT,
-   "likelihood_ratio" 	FLOAT,
-   "p_value" 		FLOAT,
-   "variance" 		FLOAT,
-   "f_stat" 		FLOAT,
-   "lod_score" 		FLOAT,
-   "cmo_name" 		VARCHAR,
-   "cmo_md5"            VARCHAR,
-   "cmo_id"             VARCHAR,
-   "vt_name" 		VARCHAR,
-   "vt_md5"             VARCHAR,
-   "vt_id"              VARCHAR,
-   "lpt_name" 		VARCHAR,
-   "lpt_md5"            VARCHAR,
-   "lpt_id"             VARCHAR,
-   "map_type" 		VARCHAR,
-   "model" 		VARCHAR,
+   "dominance_effect"	FLOAT,
+   "bayes_value"	FLOAT,
+   "likelihood_ratio"	FLOAT,
+   "p_value"		FLOAT,
+   "variance"		FLOAT,
+   "f_stat"		FLOAT,
+   "lod_score"		FLOAT,
+   "cmo_name"		VARCHAR,
+   "cmo_md5"		VARCHAR,
+   "cmo_id"		VARCHAR,
+   "vt_name"		VARCHAR,
+   "vt_md5"		VARCHAR,
+   "vt_id"		VARCHAR,
+   "lpt_name"		VARCHAR,
+   "lpt_md5"		VARCHAR,
+   "lpt_id"		VARCHAR,
+   "map_type"		VARCHAR,
+   "model"		VARCHAR,
    "flanking_markers"	VARCHAR,
    "breed"		VARCHAR,
    "pmid"		INTEGER,
@@ -32,7 +32,7 @@ CREATE TABLE "B4F"."odex4all"."QTL" (
    PRIMARY KEY ("qtl_id")
 );
 CREATE INDEX idx_QTL_chromosome ON B4F.odex4all.QTL("chromosome");
-CREATE INDEX idx_QTL_region ON B4F.odex4all.QTL("start","end");
+CREATE INDEX idx_QTL_region ON B4F.odex4all.QTL("start_pos","end_pos");
 CREATE INDEX idx_QTL_pmid ON B4F.odex4all.QTL("pmid");
 CREATE INDEX idx_QTL_cmo_md5 ON B4F.odex4all.QTL("cmo_md5");
 CREATE INDEX idx_QTL_vt_md5 ON B4F.odex4all.QTL("vt_md5");
@@ -42,7 +42,7 @@ CREATE INDEX idx_QTL_lpt_md5 ON B4F.odex4all.QTL("lpt_md5");
 CREATE TABLE "B4F"."odex4all"."ONTO" (
    "id"		VARCHAR,
    "name"	VARCHAR,
-   "name_md5"   VARCHAR,
+   "name_md5"	VARCHAR,
    PRIMARY KEY ("id")
 );
 CREATE INDEX idx_ONTO_name_md5 ON B4F.odex4all.ONTO("name_md5");
