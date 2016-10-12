65ffa7a334099e9d30965eda7b6ed1f3cc504be5
arnikz <a.kuzniar@esciencecenter.nl>
2016-10-13 00:19:49 +0200

    Rename local variables & update doc

--- B4F/src/semantify_db.sql
+++ B4F/src/semantify_db.sql
@@ -5,21 +5,24 @@
 --
 
 -- set local variables
-SET u{graph_IRI} http://localhost:8890/B4F;
-SET u{r2rml_graph_IRI} http://temp/B4F;
-SET u{r2rml_file_path} /tmp/data/r2rml.ttl;
+SET u{GRAPH_IRI} http://localhost:8890/B4F;
+SET u{R2RML_GRAPH_IRI} http://temp/B4F;
+SET u{R2RML_FILE} /tmp/share/r2rml.ttl;
+
 -- clear graphs
-SPARQL CLEAR GRAPH <$u{r2rml_graph_IRI}>;
-SPARQL DROP SILENT GRAPH <$u{graph_IRI}>;
+SPARQL CLEAR GRAPH <$u{R2RML_GRAPH_IRI}>;
+SPARQL DROP SILENT GRAPH <$u{GRAPH_IRI}>;
 
 -- store R2RML mappings in temp graph
-DB.DBA.TTLP(file_to_string('$u{r2rml_file_path}'), '$u{graph_IRI}', '$u{r2rml_graph_IRI}');
+DB.DBA.TTLP(file_to_string('$u{R2RML_FILE}'), '$u{GRAPH_IRI}', '$u{R2RML_GRAPH_IRI}');
 
 -- sanity checks
---SELECT DB.DBA.R2RML_TEST('$u{r2rml_graph_IRI}');
---DB.DBA.OVL_VALIDATE('$u{r2rml_graph_IRI}', 'http://www.w3.org/ns/r2rml#OVL');
+--SELECT DB.DBA.R2RML_TEST('$u{R2RML_GRAPH_IRI}');
+--DB.DBA.OVL_VALIDATE('$u{R2RML_GRAPH_IRI}', 'http://www.w3.org/ns/r2rml#OVL');
 
 -- convert R2RML into Virtuoso's own Linked Data Views script
-EXEC('SPARQL ' || DB.DBA.R2RML_MAKE_QM_FROM_G('$u{r2rml_graph_IRI}'));
+EXEC('SPARQL ' || DB.DBA.R2RML_MAKE_QM_FROM_G('$u{R2RML_GRAPH_IRI}'));
 
--- TODO: materialize RDF/Linked Data view(s), http://docs.openlinksw.com/virtuoso/fn_rdf_view_sync_to_physical/
+-- TODO:
+-- set R2RML_FILE variable dynamically using to docker command-line arg
+-- materialize RDF/Linked Data view(s), http://docs.openlinksw.com/virtuoso/fn_rdf_view_sync_to_physical/
