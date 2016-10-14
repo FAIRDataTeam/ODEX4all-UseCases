93062d25e3b19da47e4c556eec95dfefee65aa9d
arnikz <a.kuzniar@esciencecenter.nl>
2016-10-14 12:06:02 +0200

    Update build.sh: log execution to stdout & file

--- B4F/src/build.sh
+++ B4F/src/build.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/bin/bash -x
 
 PORT=1111
 USER=dba
 PASS=dba
-LOG_FILE=pigQTLdb.log
+LOG_FILE=$0.log
 SQL_FILES=(install_vad_pkgs.sql create_db.sql QTL.sql ONTO.sql update_db.sql semantify_db.sql)
 
 rm -f $LOG_FILE
 for fname in ${SQL_FILES[@]}
 do
     echo "### START [$(date)]: 'isql ... $fname'" >> $LOG_FILE
-    isql $PORT $USER $PASS verbose=off errors=stderr $fname &>> $LOG_FILE
+    isql $PORT $USER $PASS verbose=off errors=stdout $fname >> $LOG_FILE
     echo "### END [$(date)]" >> $LOG_FILE
 done
