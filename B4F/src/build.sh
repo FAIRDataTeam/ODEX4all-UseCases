e84cc06d127272add6251b78b860f3cfde49d87e
arnikz <a.kuzniar@esciencecenter.nl>
2016-10-25 18:08:41 +0200

    Update pigQTLdb-LD build.sh

--- B4F/src/build.sh
+++ B4F/src/build.sh
@@ -4,7 +4,7 @@ PORT=1111
 USER=dba
 PASS=dba
 LOG_FILE=$0.log
-SQL_FILES=(install_vad_pkgs.sql create_db.sql QTL.sql ONTO.sql update_db.sql semantify_db.sql)
+SQL_FILES=(install_vad_pkgs.sql create_db.sql QTL.sql ONTO.sql update_db.sql semantify_db.sql post_install_fct.sql)
 
 rm -f $LOG_FILE
 for fname in ${SQL_FILES[@]}
