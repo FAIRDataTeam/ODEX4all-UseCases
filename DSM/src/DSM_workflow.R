5b5f1ffcd178d6e9c85b3a1828e6d444e4e86db7
Anand Gavai <anand.gavai@gmail.com>
2017-01-11 12:56:25 +0100

    Implemented intersection of resistance2chemicals and 1-butanol concepts

--- DSM/src/DSM_workflow.R
+++ DSM/src/DSM_workflow.R
@@ -3,6 +3,7 @@ library(tidyr)
 library(sqldf)
 library(splitstackshape)
 library(stringr)
+library(compare)
 setwd("/home/anandgavai/AARestructure/ODEX4all-UseCases/DSM/src")
 
 ## Objective: To identify genotype-phenotype trait association in yeast
@@ -55,18 +56,29 @@ resistance2Butanol<-getIndirectRelation(start,end2)
 dfs1<-as.matrix(getTableFromJson(resistance2Chemicals))
 dfs1[,2]<-str_replace_all(dfs1[,2], "[^[:alnum:]]","")
 dfs1[,2]<-str_replace_all(dfs1[,2], "c","")
+dfs1<- data.frame(lapply(dfs1, as.character), stringsAsFactors=FALSE)
 
 
 ### unconventional way to format strings, but it works
 dfs2<-as.matrix(getTableFromJson(resistance2Butanol))
 dfs2[,2]<-str_replace_all(dfs2[,2], "[^[:alnum:]]","")
 dfs2[,2]<-str_replace_all(dfs2[,2], "c","")
+dfs2<- data.frame(lapply(dfs2, as.character), stringsAsFactors=FALSE)
+
+### Step 3: Intersect "resistance to chemicals" and "1-butanol" concepts
+comparison <- compare(dfs1,dfs2,allowAll=TRUE)
+dfs<-comparison$tM
 
 
-### Step 3: Map human redable triples from the reference database 
+
+
+### Step 4: Map human redable triples from the reference database 
+### reference list is collected from EKP
 pred<-read.csv("Reference_Predicate_List.csv",header=TRUE)
 pred<-pred[,c(2,3)]
 colnames(pred)<-c("pred","names")
+
+
 
 subject_name<-getConceptName(dfs[,"Subject"])
 dfs<-cbind(dfs,subject_name[,2])
