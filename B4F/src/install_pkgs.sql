1cc59be2fef1f8ef5c489dcf712e132d382303bd
arnikz <a.kuzniar@esciencecenter.nl>
2017-02-06 18:21:15 +0100

    RDF sponging of ontologies replaced with get-onto rule in Makefile

--- B4F/src/install_pkgs.sql
+++ B4F/src/install_pkgs.sql
@@ -10,4 +10,9 @@ SET u{PATH} /usr/local/virtuoso-opensource/share/virtuoso/vad/ ;
 VAD_INSTALL('$u{PATH}/fct_dav.vad', 0) ;
 VAD_INSTALL('$u{PATH}/rdb2rdf_dav.vad', 0) ;
 VAD_INSTALL('$u{PATH}/rdf_mappers_dav.vad', 0) ;
+
+-- grant permissions
+GRANT SPARQL_UPDATE TO "SPARQL" ;
+GRANT EXECUTE ON "DB.DBA.rdfdesc_http_url" TO "SPARQL" ;
+GRANT EXECUTE ON "DB.DBA.EXEC_AS" TO "SPARQL" ;
 QUIT;
--- B4F/src/post_install.sql
+++ B4F/src/post_install.sql
@@ -9,9 +9,5 @@ DB.DBA.VT_INC_INDEX_DB_DBA_RDF_OBJ() ;
 -- periodically populate label lookup tables
 urilbl_ac_init_db() ;
 
--- re-rank the IRIs (should be done periodically)
+-- perioducally re-rank the IRIs
 s_rank() ;
-
--- grant permissions
-GRANT SPARQL_UPDATE TO "SPARQL" ;
-GRANT EXECUTE ON "DB.DBA.rdfdesc_http_url" TO "SPARQL" ;
