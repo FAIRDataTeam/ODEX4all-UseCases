7a7f9eef244b222439dae7c0053276de9ac604cc
Anand Gavai <anand.gavai@gmail.com>
2017-02-13 12:36:30 +0100

    DTM created with terms combined together from crop ontology, keyword list and Title terms dwpi

--- Limagrain/src/tm_PatentDocs.R
+++ Limagrain/src/tm_PatentDocs.R
@@ -11,27 +11,30 @@ setwd("/home/anandgavai/AARestructure/ODEX4all-UseCases/Limagrain/src")
 
 ## url<-"http://localhost:8983/solr/terms/select"
 
-## Step 1b: I extract this dictionary from Solr here
-## dic<-solr_search(q='*:*', base=url)
+patList<-read.csv("../data/excel2016-06-21-10-09-28.csv",header = TRUE)
 
-dic<- read.csv("../data/keywords.csv",header=FALSE)
-dic<-as.character(dic[,1])
+
+## Step 1b: I extract this dictionary from Solr here
+## dic_key<-solr_search(q='*:*', base=url)
+
+dic_key<- read.csv("../data/keywords.csv",header=FALSE)
+dic_key<-as.character(dic_key[,1])
 
 ## remove leading and training white spaces
-dic<-trimws(dic)
+dic_key<-trimws(dic_key)
 
 ## remove all special characters and replace them with space
-dic <- gsub("[[:punct:]]", " ", dic)
+dic_key <- gsub("[[:punct:]]", " ", dic_key)
 
 ## remove \x96 from the character vector
-dic <- gsub("\\\x96"," ",dic)
+dic_key <- gsub("\\\x96"," ",dic_key)
 
 
 ## remove \x92 from the character vector
-dic <- gsub("\\\x92"," ",dic)
+dic_key <- gsub("\\\x92"," ",dic_key)
 
 ## remove \xd7 from the character vector
-dic <- gsub("\\\xd7"," ",dic)
+dic_key <- gsub("\\\xd7"," ",dic_key)
 
 
 ## make dictionary of crop ontologies
@@ -41,23 +44,33 @@ dic_CO<-c(as.character(dic_CO$Trait.name),as.character(dic_CO$Attribute))
 ## select only unique terms
 dic_CO<-unique(dic_CO)
 
+
+## Combined dictionary keywords + CO terms 
+dic_CO_Key<-c(dic_key,dic_CO)
+
+
+
+## I create dictionary from title terms
+title<-as.character(patList$Title...DWPI)
+ll<-NULL
+for(i in 1:length(title)){
+  ll<-c(ll,unlist(strsplit(title[i]," ")))   
+}
+
+## Combined dictionary keywords + CO terms + Title terms
+dic_CO_key_title<-c(dic_CO_Key,ll)
+dic_CO_key_title<-unique(gsub(",","",dic_CO_key_title))
+dic_CO_key_title<-unique(gsub(";","",dic_CO_key_title))
+dic_CO_key_title<-unique(gsub("\\(e.g.","",dic_CO_key_title))
+dic_CO_key_title<-unique(gsub("e.g.","",dic_CO_key_title))
+
+
 ## Step 2a: I create corpus here.
-
-patList<-read.csv("../data/excel2016-06-21-10-09-28.csv",header = TRUE)
-
-
-abst<- as.character(patList$Abstract)
 abst_dwpi<- as.character(patList$Abstract...DWPI)
-
-title_terms_dwpi<-as.character(patList$Title.Terms...DWPI)
-
 
 
 ## create corpus
-dfCorpus_abst = Corpus(VectorSource(abst)) 
 dfCorpus_abst_dwpi = Corpus(VectorSource(abst_dwpi)) 
-
-dfCorpus_title_terms_dwpi = Corpus(VectorSource(title_terms_dwpi)) 
 
 
 ## inspect the overall corpus summary
@@ -66,82 +79,40 @@ dfCorpus_title_terms_dwpi = Corpus(VectorSource(title_terms_dwpi))
 
 ## Eliminating extra white spaces
 
-reuters_abst<-tm_map(dfCorpus_abst,stripWhitespace)
-reuters_abst<-tm_map(reuters_abst,stemDocument)
-
-
 reuters_abst_dwpi<-tm_map(dfCorpus_abst_dwpi,stripWhitespace)
 reuters_abst_dwpi<-tm_map(reuters_abst_dwpi,stemDocument)
 
 
 
-reuters_title_terms_dwpi<-tm_map(dfCorpus_title_terms_dwpi,stripWhitespace)
-reuters_title_terms_dwpi<-tm_map(reuters_title_terms_dwpi,stemDocument)
-
-
 ## convert to lower case
-
-reuters_abst <- tm_map(reuters_abst, content_transformer(tolower))
 reuters_abst_dwpi <- tm_map(reuters_abst_dwpi, content_transformer(tolower))
 
-reuters_title_terms_dwpi <- tm_map(reuters_title_terms_dwpi, content_transformer(tolower))
 
 ## remove stopwords
-
-reuters_abst <- tm_map(reuters_abst, removeWords, stopwords("english"))
 reuters_abst_dwpi <- tm_map(reuters_abst_dwpi, removeWords, stopwords("english"))
-reuters_title_terms_dwpi <- tm_map(reuters_title_terms_dwpi, removeWords, stopwords("english"))
 
 
-## create a document term matrix with dic (keywords)
-dtm_abst <- DocumentTermMatrix(reuters_abst,list(dictionary=dic))
-dtm_abst_dwpi <- DocumentTermMatrix(reuters_abst_dwpi,list(dictionary=dic))
-dtm_title_terms_dwpi<-DocumentTermMatrix(reuters_title_terms_dwpi,list(dictionary=dic))
-
-
-
-## create a document term matrix with dic_CO (CO terms based on "trait name" and "attribute" terms)
-dtm_abst_CO <- DocumentTermMatrix(reuters_abst,list(dictionary=dic_CO))
-dtm_abst_dwpi_CO <- DocumentTermMatrix(reuters_abst_dwpi,list(dictionary=dic_CO))
-dtm_title_terms_dwpi_CO<-DocumentTermMatrix(reuters_title_terms_dwpi,list(dictionary=dic_CO))
+## create a document term matrix with (keywords, CO terms, title terms combined together)
+dtm_abst_dwpi_CO_Key_Title <- DocumentTermMatrix(reuters_abst_dwpi,list(dictionary=dic_CO_key_title))
 
 
 
 
 ## remove terms that occure in only 0.1% of all documents (in short less common words)
-dt_abst<-removeSparseTerms(dtm_abst, 0.99) # this is tunable 0.6 appears to be optimal
-dt_abst_dwpi<-removeSparseTerms(dtm_abst_dwpi, 0.99) # this is tunable 0.6 appears to be optimal
-dt_title_terms_dwpi<-removeSparseTerms(dtm_title_terms_dwpi, 0.99) # this is tunable 0.6 appears to be optimal
+dt_abst_dwpi_CO_Key_Title<-removeSparseTerms(dtm_abst_dwpi_CO_Key_Title, 0.99) # this is tunable 0.6 appears to be optimal
 
-## remove terms that occure in only 0.1% of all documents (in short less common words)
-dt_abst_CO<-removeSparseTerms(dtm_abst_CO, 0.99) # this is tunable 0.6 appears to be optimal
-dt_abst_dwpi_CO<-removeSparseTerms(dtm_abst_dwpi_CO, 0.99) # this is tunable 0.6 appears to be optimal
-dt_title_terms_dwpi_CO<-removeSparseTerms(dtm_title_terms_dwpi_CO, 0.99) # this is tunable 0.6 appears to be optimal
+
+row.names(dt_abst_dwpi_CO_Key_Title)<-patList$Publication.Number
 
 
 
-
-row.names(dt_abst)<-patList$Publication.Number
-row.names(dt_abst_dwpi)<-patList$Publication.Number
-row.names(dt_title_terms_dwpi)<-patList$Publication.Number
-
-row.names(dt_abst_CO)<-patList$Publication.Number
-row.names(dt_abst_dwpi_CO)<-patList$Publication.Number
-row.names(dt_title_terms_dwpi_CO)<-patList$Publication.Number
-
-
-intersect(colnames(as.matrix(dt_abst_dwpi)),colnames(as.matrix(dt_abst_dwpi_CO)))
-
-
-write.csv(as.matrix(dt_abst),file="dtm_Abstracts_CO.csv")
-write.csv(as.matrix(dt_abst_dwpi),file="dtm_Abstracts_dwpi_CO.csv")
-write.csv(as.matrix(dt_title_terms_dwpi),file="dtm_title_terms_dwpi_CO.csv")
+write.csv(as.matrix(dt_abst_dwpi_CO_Key_Title),file="dtm_Abstracts_dwpi_CO_Key_Title.csv")
 
 
 
-findFreqTerms(dt_abst, 5)
+findFreqTerms(dt_abst_dwpi_CO_Key_Title, 200)
 
-findAssocs(dt_abst_dwpi, "locus", 0.1)
+findAssocs(dt_abst_dwpi_CO_Key_Title, "kernel", 0.1)
 
 
 # to do intersect with dictionary 
