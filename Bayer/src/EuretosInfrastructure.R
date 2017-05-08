e313eb9adbf13472072e7428a5ee528734663fe6
Anand Gavai <anand.gavai@gmail.com>
2017-05-08 17:06:51 +0200

    added EKP interface

---
+++ Bayer/src/EuretosInfrastructure.R
@@ -0,0 +1,291 @@
+
+############################################
+
+#####  Setup the Infrastructure  ####
+
+###########################################
+library(httr)
+library(jsonlite)
+library(magrittr)
+library(yaml)
+library(tidyr)
+library(qdap)
+
+
+config<-yaml.load_file("../src/config.yml")
+## connect to Euretos server with login credentials of 
+login <- "http://178.63.49.197:8080/spine-ws/login/authenticate"
+pars <- list(
+  username =  config$EKP$username,
+  password =  config$EKP$password
+)
+
+res <- POST(login, body = pars,encode="json",verbose())
+
+token <- content(res)$token
+
+
+## Search for concept ids 
+# Input parameters make sure the single quotes are removed for the term
+#query = "/external/concepts/search"
+
+### Setting up the base url for Euretos Knowledge platform 
+base_url <- "http://178.63.49.197:8080/spine-ws"
+
+
+## A generic function that takes list of SGD Ids and returns back a table with SGD_Id, Concept_Id and Gene names
+### double quotes are embeded in single q
+## "quotes are not necessary inside square braces"
+out<-NULL
+getConceptID<-function(terms){
+  query = "/external/concepts/search"
+  for (i in 1:length(terms)){
+    b<-paste("{",'"queryString":"term:',as.character(terms[i]),'","searchType":"STRING"',"}",sep="")
+    pr <- POST(url = paste(base_url, query, sep =""), 
+               add_headers('X-token' = token),
+               body=fromJSON(b),
+               encode = "json", 
+               accept_json(),verbose())
+    a<-content(pr)
+    a<-cbind(terms[i],t(unlist(a)))
+    if(a[,"totalElements"]!=0){
+      out<-rbind(out,a)
+    }
+  }
+  colnames(out)<-c("geneId","EKP_Concept_Id","content.name","totalElements","totalPates","last","numberOfElements","first","size","number")
+  return(out)
+}
+
+
+
+## Template
+# { "additionalFields": ["abstract"], "ids": ["210182840"] }
+
+## Get PubmedID for each provenance id information from within EKP
+
+getPubMedId<- function(provIds){
+  out<-NULL
+  query = "/external/publications"
+  for (i in 1:length(provIds)){
+    b<-paste0("{",'"additionalFields": ["abstract"]',",",'"ids":[',provIds[i],']',"}")
+    template<-fromJSON(b,simplifyVector = FALSE,flatten=TRUE)
+    pr <- POST(url = paste(base_url, query, sep =""), 
+               add_headers('X-token' = token),
+               body=template,
+               encode = "json", 
+               accept_json(),verbose())
+    a<-content(pr)
+    id<-a[[1]]$id
+    abs<-NULL
+    if(is.null(a[[1]]$documentId)){
+      a[[1]]$documentId <- "NA"
+      abs<-cbind(id, a[[1]]$documentId)     
+    }else{
+        abs<-cbind(id, a[[1]]$documentId)    
+    }
+        out<-rbind(out,abs)
+    }
+    return(out)
+}
+
+
+
+
+
+## Template
+#{"additionalFields": ["publicationIds", "tripleIds", "predicateIds", "semanticCategory", "semanticTypes", 
+#                      "taxonomies"],
+#"positiveFilters":["sc:Chemicals & Drugs","sc:Genes & Molecular Sequences"],
+#"leftInputs":[5243207],"rightInputs":[6526817]}
+
+
+
+## Get indirect relationships at this URL
+getIndirectRelation<-function(start,end){
+  d<-NULL
+  query = "/external/concept-to-concept/indirect"
+  pages<-list()
+  for (i in 1:length(start)){
+    for (j in 1:length(end)){
+      #template<-paste0("{",'"additionalFields": ["publicationIds", "tripleIds", "predicateIds", "semanticCategory", "semanticTypes", "taxonomies"]',",",'"leftInputs":[',start[i],']',",",'"rightInputs":[',end[j],']',"}")
+      template<-paste0("{",'"additionalFields": ["publicationIds", "tripleIds", "predicateIds", "semanticCategory", "semanticTypes", "taxonomies"]',",",'"positiveFilters":["sc:Chemicals & Drugs","sc:Genes & Molecular Sequences","sc:Physiology"]',",",'"leftInputs":[',start[i],']',",",'"rightInputs":[',end[j],']',"}")
+      template<-fromJSON(template,simplifyVector = FALSE,flatten=TRUE)
+                 pr <- POST(url = paste(base_url, query, sep =""), 
+                 add_headers('X-token' = token),
+                 body=template,
+                 encode = "json", 
+                 accept_json(),verbose())
+      a<-content(pr)
+      pages[[i+1]]<-a$content
+    }
+  }
+  return(pages)
+  }
+
+
+
+anotherFormat<-function(pages){
+  re1<-NULL
+  for (i in 1:length(test[[2]])){
+    toJSON(test[[2]][[i]]) %>% select(relationships)  -> rel
+    re<- unlist(fromJSON(rel))
+    re1 <- rbind(re1,re) 
+    }  
+}
+
+
+
+
+# Function that returns table from json objects as returned by EKP
+getTableFromJson<-function(indirectRelationResultsFromEKP){
+  df<-fromJSON(toJSON(indirectRelationResultsFromEKP),flatten=TRUE)
+  do.call(rbind,df) %>% as.data.frame ->b
+  
+  ### parse only the relationships
+  rel<-b[,"relationships"]
+  
+  for (i in 1:length(rel)){
+      rel[[i]]<-c(rel[[i]],score=as.list(b$score[i]))
+  }
+  
+  ### collapse into a data frame
+  dfs<-do.call(rbind,rel)
+  colnames(dfs)<-c("Subject","Object","ekpTripleID","publicationIds","Predicate","Score")
+  
+  dfs<-as.data.frame(dfs)
+  ### Select subject,predicate and object columns
+  dfs<-cbind(unlist(dfs[,"Subject"]),unlist(dfs[,"Object"]),as.character.default(unlist(dfs[,"Predicate"])),as.character.default(unlist(dfs[,"publicationIds"])),unlist(dfs[,"Score"]))
+  colnames(dfs)<-c("Subject","Object","Predicate","Publications","Score")
+  dfs<-dfs[,c(1,3,2,4,5)]
+  dfs<-cSplit(dfs,"Predicate",",","long")
+  dfs<-cSplit(dfs,"Publications",",","long")
+}
+
+
+### Function to retrieve resistance to chemicals
+getResistanceEKPID<-function(){
+  query="/external/concepts/search"
+  template<-paste("{",'"queryString":"term:',"'apo:0000087'",'","searchType":"STRING"',"}",sep="")
+  pr <- POST(url = paste(base_url, query, sep =""), 
+             add_headers('X-token' = token),
+             body=fromJSON(template),
+             encode = "json", 
+             accept_json(),verbose())
+  a<-content(pr)
+}
+
+
+### Function to retrieve butanol tolerance
+getButanolID<-function(){
+  query="/external/concepts/search"
+  template<-paste("{",'"queryString":"term:',"'c0089147'",'","searchType":"STRING"',"}",sep="")
+  pr <- POST(url = paste(base_url, query, sep =""), 
+             add_headers('X-token' = token),
+             body=fromJSON(template),
+             encode = "json", 
+             accept_json(),verbose())
+  a<-content(pr)
+}
+
+
+
+## Get Concept name from concept id
+out<-NULL
+getConceptName<-function(ids){
+  query<- "/external/concepts"
+  for (i in 1:length(ids)){
+    b<-paste0("{",'"ids":','["',as.character(ids[i]),'"]',"}",sep="")
+    pr <- POST(url = paste(base_url, query, sep =""), 
+               add_headers('X-token' = token),
+               body=fromJSON(b,simplifyVector = FALSE),
+               encode = "json", 
+               accept_json(),verbose())
+    a<-content(pr)
+    #print (a)
+    a<-do.call(rbind, lapply(a, data.frame, stringsAsFactors=FALSE))
+    out<-rbind(out,a)
+  }
+  return(out)
+}
+
+
+
+## Get predicate name predicate
+getPredicateName<-function(){
+  query = "/external/predicates"
+  pr <- POST(url = paste(base_url, query, sep =""), 
+             add_headers('X-token' = token),
+             body=NULL,
+             encode = "json", 
+             accept_json(),verbose())
+  a<-content(pr)
+  pages<-list()
+  for (i in 0:a$totalPages){
+    pr <- POST(url = paste(base_url, query,"?page=",i ,sep =""), 
+               add_headers('X-token' = token),
+               body=NULL,
+               encode = "json", 
+               accept_json(),verbose())
+    b<-content(pr)
+    message("Retrieving page ", i)
+    pages[[i+1]]<-b$content
+  }
+  return(pages)
+}
+
+###################################
+
+### Get Predicate names #########
+predicates<- getPredicateName()
+
+pred<-fromJSON(toJSON(predicates),flatten = TRUE)
+mat<-NULL
+for (i in 1:length(pred)){
+  id<-unlist(pred[[i]]$id)  
+  name<-unlist(pred[[i]]$name)
+  t<-cbind(id,name)
+  mat<-rbind(mat,t)
+}
+mat<-as.data.frame(mat)
+
+#### Create predicate list for future reference
+write.csv(mat,file="Reference_Predicate_List.csv")
+
+
+##########################################################
+
+#           Examples to run queries                      #
+##########################################################
+#query<- "/external/semantic-categories"
+#gr = GET(
+#  url = paste(base_url, query, sep =""),
+#  add_headers('X-token' = token),
+#  body=fromJSON('{"additionalFields": ["egfr"]}'),
+#  encode = "json",
+#  accept_json()
+#)
+
+#content(gr)
+
+
+### Concept-to-concept direct/indirect query example that works ###############
+#template<-fromJSON('{
+#  "additionalFields": [
+#    "publicationIds", "tripleIds", "predicateIds", "semanticCategory", "semanticTypes"
+#    ],
+#  "leftInputs": [
+#    "start"
+#    ],
+#  "relationshipWeightAlgorithm": "PWS",
+#  "rightInputs": [
+#    "end"
+#    ],
+#  "sort": "ASC"
+#}')
+
+### Fetch the concept id at EKP make sure there are single quotes in the search when it has spaces in it.
+# { "queryString": "term:a-maltose", "searchType": "STRING" } 1280049
+# { "queryString": "term:'glucose measurement'", "searchType": "STRING" } 3416536
+
+
+#5258250 # resistance to chemicals concept id
--- Bayer/src/Workflow_Rice.R
+++ Bayer/src/Workflow_Rice.R
@@ -1,34 +1,50 @@
-library("biomaRt")
-listMarts(host="plants.ensembl.org")
-gramene = useMart("plants_mart", dataset="osativa_eg_gene", host="plants.ensembl.org")
-listDatasets(gramene)
-gramene = useDataset("osativa_eg_gene",mart=gramene)
-filters = listFilters(gramene)
-attributes = listAttributes(gramene)
-### collect gene specific information
-attributes = attributes[grep("gene",attributes[,1]),]
+library(dplyr)
+library(tidyr)
+library(sqldf)
+library(splitstackshape)
+library(stringr)
+library(compare)
+detach(package:RMySQL)
 
+
+## Objective: To identify genotype-phenotype trait association in yeast
+### Develop a workflow to identify genes indirectly associated with a certain yeast phenotype (butanol tolerance) using EKP and visualize them in an interactive knowledge graph.
+
+source("..//src/EuretosInfrastructure.R")
+options(warn=-1)
 
 #### qtaro.abr.affrc.go.jp/qtab/table
 setwd("~/odex4all_usecases/ODEX4all-UseCases/Bayer/data")
 
-gitQ <-read.csv("GeneInformationTable_Qtaro.csv",header=TRUE)
-
-qtlQ<-read.csv("QTLInformationTable_Qtaro.csv",header=TRUE)
+rice_genes <-read.csv("GeneInformationTable_Qtaro.csv",header=TRUE)
 
 
-## search for grain size and grain number
-
-terms selected
-c("grain size","grain thickness","grain number","kernel number","GRNB","fruit number","grain number per plant","GN")
-
-https://www.biostars.org/p/66494/
-
-
-
-
-
+### Select only morphological trait as these are associated with concept ids are dynamic (snapsnot date: 08-05-2017)
+# "grain size" (EKP concept id : 5899980)
+#"grain thickness" (EKP concept id  :5900661)
+#"grain number" (EKP concept id (rice specific) :4343608)
+#"kernel number" (EKP concept id:5900190)
+#"GRNB" (EKP concept:5900394)
+#"fruit number" (EKP concept:5900077)
+#"grain number per plant" (EKP concept (exact): 5900828)
+#"GN" (EKP concept:(vague many hits within EKP))
 
 
+rice_genes <- select(rice_genes,gene_symbol,character_major)  
+rice_genes<- filter(rice_genes, character_major == "Morphological trait")
+rice_genes<- tolower(as.character(rice_genes[,"gene_symbol"]))
+rice_genes <- unique(rice_genes)
 
+
+rice_genes<-gsub(";","",rice_genes)
+rice_genes<-gsub("-","",rice_genes)
+
+## Filter for grain size and grain number is not done yet 
+### terms_selected<- c("grain size","grain thickness","grain number","kernel number","GRNB","fruit number","grain number per plant","GN")
+
+## Step 1a : Get the starting concept identifiers
+## start<-getStartConceptID(as.character(yeast_genes[,1]))
+getConceptID(head(rice_genes,n=1))
+
+start<-getConceptID(head(rice_genes,n=10)))
 
