4913d78a6a3bdb6be1d8fd6bb3f11d2310fd96ee
Anand Gavai <anand.gavai@gmail.com>
2017-05-09 17:30:24 +0200

    first results

--- Bayer/src/Workflow_Rice.R
+++ Bayer/src/Workflow_Rice.R
@@ -36,15 +36,59 @@ rice_genes<- tolower(as.character(rice_genes[,"gene_symbol"]))
 rice_genes <- unique(rice_genes)
 
 
-rice_genes<-gsub(";","",rice_genes)
-rice_genes<-gsub("-","",rice_genes)
 
 ## Filter for grain size and grain number is not done yet 
 ### terms_selected<- c("grain size","grain thickness","grain number","kernel number","GRNB","fruit number","grain number per plant","GN")
 
 ## Step 1a : Get the starting concept identifiers
-## start<-getStartConceptID(as.character(yeast_genes[,1]))
-getConceptID(head(rice_genes,n=1))
 
-start<-getConceptID(head(rice_genes,n=10)))
+start<-getConceptID(rice_genes)
+start<-start[,"EKP_Concept_Id"]
 
+
+## Step 1b: Get the ending concept identifiers for "resistance to chemicals"
+end <- unlist(getTraitEKPID())
+end<-end["content.id"] #EKP ID of resistance to chemicals
+
+
+
+## Step 2a: Get Indirect relationships between "rice genes"(start) and "grain number"(end)
+genes2GrainNumber<-getIndirectRelation(start,end)
+save(genes2GrainNumber, file = "genes2GrainNumber.rda")
+
+
+load("genes2GrainNumber.rda")
+
+### Formatting and data cleaning
+dfs<-as.matrix(getTableFromJson(genes2GrainNumber))
+dfs[,"Predicate"]<-str_replace_all(dfs[,"Predicate"], "[^[:alnum:]]","")
+dfs[,"Predicate"]<-str_replace_all(dfs[,"Predicate"], "c","")
+dfs[,"Publications"]<-str_replace_all(dfs[,"Publications"], "[^[:alnum:]]","")
+dfs[,"Publications"]<-str_replace_all(dfs[,"Publications"], "c","")
+dfs<- data.frame(dfs, stringsAsFactors=FALSE)
+
+
+### Step 4: Map human redable triples from the reference database 
+### reference list is collected from EKP
+pred<-read.csv("Reference_Predicate_List.csv",header=TRUE)
+pred<-pred[,c(2,3)]
+colnames(pred)<-c("pred","names")
+
+
+subject_name<-getConceptName(dfs[,"Subject"])
+dfs<-cbind(dfs,subject_name[,2])
+
+object_name<-getConceptName(dfs[,"Object"])
+dfs<-cbind(dfs,object_name[,2])
+
+predicate_name<-sqldf('select * from dfs left join pred on pred.pred=dfs.Predicate')
+
+pbs<-getPubMedId(dfs$Publications)
+
+tripleName<-cbind(subject_name[,"name"],as.character(predicate_name[,"names"]),object_name[,"name"],dfs[,"Publications"],dfs[,"Score"])
+colnames(tripleName)<-c("Subject","Predicate","Object","Provenance","Score")
+
+write.table(tripleName,file="/home/anandgavai/odex4all_usecases/ODEX4all-UseCases/Bayer/src/triples.csv",sep=",",row.names = FALSE)
+
+
+### cross validation
